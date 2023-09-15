06026bcf13543123d34d44acebe3da1471b78d9f
Deepa@1998# <deepanjalisamantaray123@gmail.com>
2023-09-15 18:01:59 +0530

    new file created

---
+++ J.js
@@ -0,0 +1,7 @@
+alert("Welcome to my Page")
+var name1=prompt("Enter your name :")
+var age1=prompt("Enter your age")
+var days=365*age1
+var remaining=(75-age1)*365
+alert("Your name is"+name1+"you lived"+days+"days and your having"+remaining+"days...")
+
---
+++ K.js
@@ -0,0 +1,38 @@
+alert("Welcome to my Webpage")
+var age1=Number(prompt("Enter your age :"))
+if(age1>=1 && age1<18)
+{
+    alert("Study Well")
+}
+else if(age1>=18 && age1<28)
+{
+    alert("Get a Job")
+}
+else if(age1>=28 && age1<32)
+{
+    alert("Marry Soon")
+}
+else if(ag>=32 && age1<40)
+{
+    alert("Have a Baby")
+}
+else if(age1>=40 && age1<55)
+{
+    alert("Earn Money")
+}
+else if(age1>=55 && age1<65)
+{
+    alert("Visit Place")
+}
+else if(age1>=65 && age1<70)
+{
+    alert("Pray God")
+}
+else if(age1>=70 && age1<75)
+{
+    alert("Drink Alchol")
+}
+else
+{
+    alert("Mari Jaa")
+}
